9cc933d63feaf1862c28627376ccb45fe8f1d40c
rorxtto <pedrohenriqueroratto@gmail.com>
2023-06-12 20:19:22 -0300

    implementacao js

---
+++ Cadastro_Guia/main.js
@@ -0,0 +1,14 @@
+function validarEmail() {
+    const emailInputs = document.getElementsByName("inputemail");
+    const emailInput = emailInputs[0];
+    const email = emailInput.value;
+  
+    if (email.includes("@")) {
+      alert("O e-mail é válido.");
+    } else {
+      alert("O e-mail é inválido.");
+    }
+  }
+
+
+  
---
+++ Cadastro_Turista/main.js
@@ -0,0 +1,11 @@
+function validarEmail() {
+    const emailInputs = document.getElementsByName("inputemail");
+    const emailInput = emailInputs[0];
+    const email = emailInput.value;
+  
+    if (email.includes("@")) {
+      alert("O e-mail é válido.");
+    } else {
+      alert("O e-mail é inválido.");
+    }
+  }
